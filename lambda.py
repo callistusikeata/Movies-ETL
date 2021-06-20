a1234c99196489f0da6a16c2c0ae871c71f8d2e9
callistusikeata <82552594+callistusikeata@users.noreply.github.com>
2021-06-20 15:25:04 -0400

    Commit to main

---
+++ lambda.py
@@ -0,0 +1,10 @@
+def make_snacks(protein, topping):
+    snacks = f"Here is a snacks with {protein} and {topping}"
+    print(snacks)
+
+make_snacks('beef', 'tomatoes')
+make_snacks('fish', 'almond')
+
+
+
+
